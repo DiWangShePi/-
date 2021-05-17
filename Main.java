b0f2a883c7e41f6ea8e70fa70f105395930b33e0
DiWangShePi <83491150+DiWangShePi@users.noreply.github.com>
2021-05-17 15:22:50 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import MainFrame.MainFrame;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+
+            MainFrame mainFrame = new MainFrame();
+
+        });
+    }
+}
